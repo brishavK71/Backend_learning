50d4dbf33a2cc55b77f541ce4bf36ff4d11c5548
brishavK71 <brishavk71@gmail.com>
2024-02-21 22:58:41 +0545

    db connection

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require("dotenv").config({ path: "./env" });
+import connectDB from "./db/dbconnect.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+// import express from "express";
+
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR:", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR:", error);
+//     throw err;
+//   }
+// })();
