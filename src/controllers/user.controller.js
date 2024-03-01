7510bbf4b6160ec1fd6a4c673f8dbaad8b99d386
brishavK71 <brishavk71@gmail.com>
2024-03-01 15:57:36 +0545

    user.controller.js halfway throw the video

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,32 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+//get user details from frontend
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok", // message should be passed as a string encoded. This here was an error
+  const { fullName, email, username, password } = req.body;
+  console.log("email :", email);
+
+  //begineers method
+  // if (fullName === "") {
+  // throw new ApiError(400, "fullname is required")
+  // }
+
+  //Validation check
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  //check if the user already exists
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(409, "user with email or username already exists");
+  }
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  // for file handling the code below allows user to upload images by passing through multer middelware
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser,
+);
 
 export default router;
