1b737fd2e7143f2271ec02853361d203bfdbf4c9
brishavK71 <brishavk71@gmail.com>
2024-03-11 15:58:15 +0545

    user log in and log out controller, cookies and auth middleware

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-  }),
+  })
 );
 
 // CONFIGURATION FOR JSON ACCEPTANCE LIMIT
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
+// forgot .js
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
